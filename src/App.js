0eeb0c15475700e9000c3c4f542c9f22ea73236b
Monika Maurya <96534851+monikamaurya77@users.noreply.github.com>
2023-05-03 18:32:07 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import Login from './components/Login';
 import Home from './components/Home';
 import { AuthContext } from './components/contextApi/context';
 import React,{ useContext } from 'react';
-import {Routes, Route} from 'react-router-dom';
-import  SeeMoreMovies from './components/SeeMoreMovies';
-import MovieDetails from './components/MovieDetails';
+import {Routes, Route,  Navigate } from 'react-router-dom'
+import MoviesDetails from './components/MoviesDetails';
+import MovieCard from './components/MovieCard';
+import Drama from './components/sidebar/Drama';
+import Action from  './components/sidebar/Action';
+import Allmovies from './components/sidebar/Allmovies';
+import Comedy from './components/sidebar/Comedy';
+import TopRated from './components/TopRated';
 
 function App() {
 
@@ -18,9 +23,16 @@ const {login} = useContext(AuthContext);
       } */}
       
       <Routes>
-        <Route path="/" element={login ? (<Home/>) : (<Login/>)}/>
-        <Route path="/moremovies" element={<SeeMoreMovies/>} />
-        <Route path="/moviedetail" element={<MovieDetails/>} />
+      <Route path="/" element={login ? <Home /> : <Login />}>
+          <Route path="" element={<Navigate to="toprated" />} />{" "}
+          <Route path="toprated" element={login ? <MovieCard /> : <Login />} />{" "}
+          <Route path="allmovies" element={login ? <Allmovies /> : <Login />} />{" "}
+          <Route path="comedy" element={login ? <Comedy /> : <Login />} />{" "}
+          <Route path="action" element={login ? <Action /> : <Login />} />{" "}
+          <Route path="drama" element={login ? <Drama /> : <Login />} />{" "}
+        </Route>
+        <Route
+          path="/MoviedetailPage/:id" element={login ? <MoviesDetails /> : <Login />} />{" "}
       </Routes>
     </div>
   );
